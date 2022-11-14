b703a58d2ed73277b830b4bf7d3a522784179b25
ratlord <bennett.daniel@gmail.com>
2022-11-14 18:44:48 -0500

    minor select styling, questions data object array

---
+++ src/assets/data/questions.js
@@ -0,0 +1,79 @@
+export const questions = [
+  {
+    id:1,
+    primaryQuestion:'Have hair color',
+    primaryValue:'hairColor',
+    secondaryOptions:['black', 'brown', 'blonde', 'red', 'white'],
+    secondaryValues:['black', 'brown', 'blonde', 'red', 'white'],
+  },
+  {
+    id:2,
+    primaryQuestion:'Have gender of',
+    primaryValue:'sex',
+    secondaryOptions:['male', 'female'],
+    secondaryValues:['m','f']
+  },
+  {
+    id:3,
+    primaryQuestion:'Have ___ lips',
+    primaryValue:'lipSize',
+    secondaryOptions:['big', 'small'],
+    secondaryValues:['big', 'small'],
+  },
+  {
+    id:4,
+    primaryQuestion:'Have ___ nose',
+    primaryValue:'noseSize',
+    secondaryOptions:['big', 'small'],
+    secondaryValues:['big', 'small'],
+  },
+  {
+    id:5,
+    primaryQuestion:'Have ___ eyes',
+    primaryValue:'eyeColor',
+    secondaryOptions:['brown', 'blue'],
+    secondaryValues:['brown', 'blue'],
+  },
+  {
+    id:6,
+    primaryQuestion:'Have ___ lips',
+    primaryValue:'lipSize',
+    secondaryOptions:['big', 'small'],
+    secondaryValues:['big', 'small'],
+  },
+  {
+    id:7,
+    primaryQuestion:'Have facial hair',
+    primaryValue:'facialHair',
+  },
+  {
+    id:8,
+    primaryQuestion:'Have red cheeks',
+    primaryValue:'redCheeks',
+  },
+  {
+    id:9,
+    primaryQuestion:'Have curly hair',
+    primaryValue:'curlyHair',
+  },
+  {
+    id:10,
+    primaryQuestion:'Wear Glasses',
+    primaryValue:'glasses',
+  },
+  {
+    id:11,
+    primaryQuestion:'Wear a hat',
+    primaryValue:'hat',
+  },
+  {
+    id:12,
+    primaryQuestion:'Have some balding',
+    primaryValue:'bald',
+  },
+  {
+    id:13,
+    primaryQuestion:'Have bushy eyebrows',
+    primaryValue:'bushyEyebrows',
+  },
+]
